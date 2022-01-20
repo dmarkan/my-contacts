8421e6b3fbeff7024f26ee5d6bfdf9f709e052c5
Darko Markan <75615049+dmarkan@users.noreply.github.com>
2022-01-20 17:32:08 +0100

    first commit

---
+++ src/contacts.js
@@ -0,0 +1,25 @@
+const contacts = [
+    {
+      name: "Beyonce",
+      imgUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Beyonc%C3%A9_Black_Is_King_Still.png/536px-Beyonc%C3%A9_Black_Is_King_Still.png",
+      phone: "+123 456 789",
+      email: "b@beyonce.com"
+    },
+    {
+      name: "Jack Bauer",
+      imgUrl: "https://upload.wikimedia.org/wikipedia/en/b/b9/Jack_Bauer.jpg",
+      phone: "+987 654 321",
+      email: "jack@nowhere.com"
+    },
+    {
+      name: "Chuck Norris",
+      imgUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Chuck_Norris_May_2015.jpg/439px-Chuck_Norris_May_2015.jpg",
+      phone: "+918 372 574",
+      email: "gmailk@chucknorris.com"
+    }
+  ];
+  
+  export default contacts;
+  
